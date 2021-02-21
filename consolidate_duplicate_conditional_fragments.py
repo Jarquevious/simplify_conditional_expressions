9c94bd6aca134729b5cbe978764baf8a59a7aa52
Jarquevious <jarquevious@gmail.com>
2021-02-20 23:41:26 -0800

    consolidate duplicate conditional fragments complete

--- consolidate_duplicate_conditional_fragments.py
+++ consolidate_duplicate_conditional_fragments.py
@@ -12,21 +12,20 @@ def make_drink(drink, addons):
     mix = []
     mix = add(mix, drink)
     mix = add(mix, addons)
+    if 'strawberry milkshake' in drink:
+        mix = mixer_ice_with_cream()
     return mix
 
 drink=[]  
 if 'coffee' in drink:
        make_drink()
 
-if 'strawberry milkshake' in drink:
-        mix = mixer_ice_with_cream()
-        make_drink()
        
 
 
 
-final_drink = make_drink(drink='coffee',addons='none'),
-# milk_shake = make_drink(drink='strawberry milkshake', addons=['milk', 'sugar'])
+# final_drink = make_drink(drink='coffee',addons='none'),
+milk_shake = make_drink(drink='strawberry milkshake', addons=['milk', 'sugar'])
 
-print(final_drink)
-# print(milk_shake)
+# print(final_drink)
+print(milk_shake)
