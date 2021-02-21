2d14c7d519455ca29e80c75b4c18d28ae6dd3bc8
Jarquevious <jarquevious@gmail.com>
2021-02-21 14:45:21 -0800

    move_field.py

--- move_field.py
+++ move_field.py
@@ -2,7 +2,10 @@
 # Move Field (attribute)
 class Gun:
     def __init__(self, name):
-        self.name = name        
+        self.name = name
+        self.num_cathridge_bullets = []
+        # initialize with 10 bullets in each gun's cathridge.
+        self.num_cathridge_bullets.append(10)          
     def shoot(self):
         print('shoot')
     def reload(self):
@@ -11,16 +14,12 @@ class Gun:
 class Player:
     def __init__(self, guns):        
         self.guns = guns
-        self.num_cathridge_bullets = [] # corresponding list that holds number of bullets for 
-                                        # each gun in the 'self.guns'
-        for _ in range(len(guns)):
-            # initialize with 10 bullets in each gun's cathridge.
-            self.num_cathridge_bullets.append(10) 
+        
     def fire_once(self):
         for i, gun in enumerate(self.guns):
-            if self.num_cathridge_bullets[i] > 0:
+            if gun.num_cathridge_bullets[i] > 0:
                 gun.shoot()
-                self.num_cathridge_bullets[i] -= 1
+                gun.num_cathridge_bullets[i] -= 1
                 break
             
 def game_loop():
