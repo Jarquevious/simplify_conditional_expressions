caa6610ee722614fa2ce2e43299aac3cebdf5f80
Jarquevious <jarquevious@gmail.com>
2021-02-20 23:31:46 -0800

    consolidate duplicate conditional fragment hlf complete

--- consolidate_duplicate_conditional_fragments.py
+++ consolidate_duplicate_conditional_fragments.py
@@ -10,20 +10,23 @@ def mixer_ice_with_cream():
 
 def make_drink(drink, addons):
     mix = []
-    mix = add(mix, '')
+    mix = add(mix, drink)
     mix = add(mix, addons)
-
-    
-    if 'coffee' in drink:
-        mix = []
-        mix = add(mix, 'coffee')
-        mix = add(mix, addons)
-    if 'strawberry milkshake' in drink:
-        mix = []
-        mix = mixer_ice_with_cream()
-        mix = add(mix, 'strawberry')
-        mix = add(mix, addons)
     return mix
 
-final_drink = make_drink('strawberry milkshake', ['milk','sugar'])
+drink=[]  
+if 'coffee' in drink:
+       make_drink()
+
+if 'strawberry milkshake' in drink:
+        mix = mixer_ice_with_cream()
+        make_drink()
+       
+
+
+
+final_drink = make_drink(drink='coffee',addons='none'),
+# milk_shake = make_drink(drink='strawberry milkshake', addons=['milk', 'sugar'])
+
 print(final_drink)
+# print(milk_shake)
